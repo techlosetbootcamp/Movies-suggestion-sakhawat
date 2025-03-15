2a75429fc97e09049d6007525e0e68921394fd2e
Sakhawat4847 <sakhawat4847@gmail.com>
2025-03-15 12:12:56 +0500

    add more movies in popular release

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             </div>
           </div>
         )}
-        {!loading && <PopularReleases className="mt-6" />}
+        {!loading && <PopularReleases />}
       </div>
     </div>
   );
